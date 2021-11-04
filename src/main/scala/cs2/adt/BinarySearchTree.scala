bf1c1ebd6e8276ce4edf4d5f745d6941b149a5f4
Matt Hibbs <mhibbs@trinity.edu>
2021-11-04 13:50:41 -0500

    Finished binary search tree

--- src/main/scala/cs2/adt/BinarySearchTree.scala
+++ src/main/scala/cs2/adt/BinarySearchTree.scala
@@ -23,6 +23,32 @@ class BinarySearchTree[A <% Ordered[A]] {
         else right = new Node(elem, null, null)
       }
     }
+    private def passUpMaxKid():(A,Node) = {
+      if(right == null) (data, left)
+      else {
+        val (d,n) = right.passUpMaxKid()
+        right = n
+        (d, this)
+      }
+
+    }
+    def remove(elem:A):Node = {
+      if(data >= elem && data <= elem) { //if equal
+        if(left == null) right
+        else if(right == null) left
+        else {
+          val (d,n) = left.passUpMaxKid()
+          left = n
+          data = d
+          this
+        }
+      } else {
+        if(elem < data) left = left.remove(elem)
+        else right = right.remove(elem)
+        this
+      }
+    }
+
   }
 
   private var root:Node = null
@@ -34,6 +60,8 @@ class BinarySearchTree[A <% Ordered[A]] {
     if(root == null) root = new Node(elem, null, null)
     else root.insert(elem)
   }
-  def remove(elem:A):Unit = ???
+  def remove(elem:A):Unit = {
+    if(root != null) root = root.remove(elem)
+  }
   def isEmpty():Boolean = { root == null }
 }
